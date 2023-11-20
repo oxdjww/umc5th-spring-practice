a9c30ad8465beb5ee93369e8c38940ab75ce6ded
oxdjww <oxdjww@gmail.com>
2023-11-20 19:22:12 +0900

    feat : 양방향 매핑 설정

--- src/main/java/umc/spring/domain/Inquiry.java
+++ src/main/java/umc/spring/domain/Inquiry.java
@@ -3,9 +3,11 @@ package umc.spring.domain;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -33,6 +35,7 @@ public class Inquiry extends BaseEntity {
     @Column(columnDefinition = "TIMESTAMP", updatable = false)
     private LocalDateTime inquiryTime;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 }
